84b9eb0f5cb71fe50d84231161924f3c90041e41
vikrant dubey <Vikrant2003aug06@gmail.com>
2025-07-17 02:36:56 +0530

    BMI Calculator

--- Project_2/script.js
+++ Project_2/script.js
@@ -6,16 +6,25 @@ form.addEventListener('submit',function(e){
    const weight = parseInt(document.querySelector('#weight').value)
    const results = document.querySelector('#results')
 
-   if(height === '' || height < 0 || isNaN(height) ){
+   if(height === '' || height <= 0 || isNaN(height) ){
         results.innerHTML = `Please give a valid height ${height}`
    }
-  if(weight === '' || weight < 0 || isNaN(weight) ){
+  if(weight === '' || weight <= 0 || isNaN(weight) ){
         results.innerHTML = `Please give a valid weight ${weight}`
    }
    else{
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
-   }
+    let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result and message
+    let message = ''
+    if (bmi < 18.6){
+        message = 'Under Weight (Less than 18.6)';}
 
-})
+        else if (bmi >= 18.6 && bmi <= 24.9) {
+      message = 'Normal Range (18.6 - 24.9)';}
+
+       else 
+        {message = 'Overweight (Greater than 24.9)';}
+       results.innerHTML = `<span> your BMI is ${bmi} </span> <p>${message}</p>`
+    }
+   }
+)
